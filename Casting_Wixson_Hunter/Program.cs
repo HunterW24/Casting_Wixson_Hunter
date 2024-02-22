ab1d0e1275b8f7a9e1d82cda2e02fd446e0023a1
HunterW <146263396+HunterW24@users.noreply.github.com>
2024-02-22 13:47:07 -0600

    Update Program.cs

--- Casting_Wixson_Hunter/Program.cs
+++ Casting_Wixson_Hunter/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Ask the user to input a whole number
+        Console.WriteLine("Please enter a whole number:");
+        int userNumber = Convert.ToInt32(Console.ReadLine());
+
+        // Print out the user's favorite number
+        Console.WriteLine("Your favorite number is " + userNumber + "!");
+
+        // Ask the user a yes or no question
+        Console.WriteLine("Do you like video games? (Enter 'true' or 'false'):");
+        bool isTrue = Convert.ToBoolean(Console.ReadLine());
+
+        // Print out the message based on the user's response
+        Console.WriteLine("It is " + isTrue + " that I like video games!");
+    }
+}
